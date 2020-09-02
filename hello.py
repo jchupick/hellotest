95028ddda0efa5eb404972b20465e61a3f49bd74
JB Chupick <jchupick@gmail.com>
2020-09-02 15:02:49 -0400

    Enable API endpoints to make calls to psutil module

--- hello.py
+++ hello.py
@@ -1,10 +1,44 @@
+import json
+import time
+import psutil
 from flask import Flask
+
 app = Flask(__name__)
 
+START_TIME = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+_VER = '1.0.0'
+
+json_out =  {
+                "status"  : 'OK', 
+                "version" : _VER, 
+                "uptime"  : 'up since ' + START_TIME, 
+                "memory"  : dict(psutil.virtual_memory()._asdict())
+            }
+
 @app.route('/')
-def index():
-    return 'Hello from root'
+def return_index():
+    return 'Hello!'
   
-@app.route('/greet')
-def say_hello():
-    return 'Hello from greet directory'
+@app.route('/healthz')
+def return_healthz():
+    return json.dumps(json_out, indent=4)
+
+@app.route('/version')
+def return_version():
+    return 'Version is ' + _VER
+
+@app.route('/mem')
+def return_mem():
+    return json.dumps(dict(psutil.virtual_memory()._asdict()), indent=4)
+
+@app.route('/disk')
+def return_disk():
+    return json.dumps(dict(psutil.disk_usage('/')._asdict()), indent=4)
+
+@app.route('/help')
+def return_help():
+    return 'Help here...'
+
+# Invokation from Docker image starts from here
+if __name__ == "__main__": 
+    app.run(host ='0.0.0.0', port = 8080, debug = True)
