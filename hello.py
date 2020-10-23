5ee848bf6e0c593167f15f6f41d6316fa22a909d
James (JB) Chupick <31314756+jchupick@users.noreply.github.com>
2020-10-23 17:28:47 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -78,4 +78,4 @@ def return_help():
 
 # Invokation from Docker image starts from here
 if __name__ == "__main__": 
-    app.run(host ='0.0.0.0', port = 8080, debug = False)
+    app.run(host ='0.0.0.0', debug = False)
